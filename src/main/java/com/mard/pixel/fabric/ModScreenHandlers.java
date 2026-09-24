84864b1641d1a481b5cd5b354aa273cb5ac10c67
QWERTYUIOP733 <sgsy_0813152546728@qq.com>
2026-09-24 20:43:54 +0800

    Fix MenuType constructor with FeatureFlagSet parameter

--- src/main/java/com/mard/pixel/fabric/ModScreenHandlers.java
+++ src/main/java/com/mard/pixel/fabric/ModScreenHandlers.java
@@ -2,6 +2,7 @@ package com.mard.pixel.fabric;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 
 /**
@@ -14,6 +15,6 @@ public class ModScreenHandlers {
         MARD_CRAFTING_TABLE = Registry.register(
                 BuiltInRegistries.MENU,
                 MardPixelMod.id("mard_crafting_table"),
-                new MenuType<MardCraftingScreenHandler>((syncId, playerInventory) -> new MardCraftingScreenHandler(syncId, playerInventory)));
+                new MenuType<>((syncId, playerInventory) -> new MardCraftingScreenHandler(syncId, playerInventory), FeatureFlags.DEFAULT_FLAGS));
     }
 }
